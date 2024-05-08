5ff8dbc07724ce1d3c88facdfa8be68f544beb84
lsh02002 <lsh02002@hanmail.net>
2024-05-08 17:57:23 +0900

    //참고한 부분이 있어서 알려 드리려고 주석을 수정했습니다

--- src/main/java/com/github/supercodingspring/repository/reservations/ReservationJpaRepository.java
+++ src/main/java/com/github/supercodingspring/repository/reservations/ReservationJpaRepository.java
@@ -19,6 +19,9 @@ public interface ReservationJpaRepository extends JpaRepository<Reservation, Int
            "WHERE p.user.userId = :userId ")
     List<FlightPriceAndCharge> findFlightPriceAndCharge(Integer userId);
 
+    //아래 내용부터 과제 내용인데
+    //솔직히 감이 오지 않아서 참고를 했습니다.
+
     @Query("SELECT DISTINCT f.arrivalLocation FROM Reservation r " +
             "JOIN r.passenger p " +
             "JOIN r.airlineTicket a " +
